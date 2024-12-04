2b41da55ece5054eace201eba2ada73283740dc5
agneselimbi <44297854+agneselimbi@users.noreply.github.com>
2024-12-04 10:14:51 -0500

    add the cypress e2e test for backend
    Created e2e test for the post and get resource behaviour.

---
+++ scripts/cypress/e2e/api.cy.js
@@ -0,0 +1,64 @@
+describe("test api", ()=>{
+    it('test get resource', ()=>{
+        cy.request('GET',"https://3hj9vc9684.execute-api.us-west-1.amazonaws.com/dev/getVisitorCount")
+        .then((response)=>{
+            //validate the status code 
+            expect(response.status).to.eq(200);
+            //validate the response body
+            expect(response.body).to.have.property('total_visitor_count')
+        });
+        cy.request({
+            method:'GET',
+            url:"https://3hj9vc9684.execute-api.us-west-1.amazonaws.com/dev/getisitorCount",
+            failOnStatusCode:false
+        })
+        .then((response) => {
+            //validate the status code 
+            expect(response.status).to.eq(403);
+        });
+    })
+    
+    it('test post resource', ()=>{
+        cy.request('POST',"https://3hj9vc9684.execute-api.us-west-1.amazonaws.com/dev/incrementVisitorCount")
+        .then((postresponse)=>{
+            //validate the status code 
+            expect(postresponse.status).to.eq(200);
+            //validate ip address
+            expect(postresponse.body).to.deep.eq({message:"Visitor added to table succesfully !"});
+        });
+        cy.request('GET',"https://3hj9vc9684.execute-api.us-west-1.amazonaws.com/dev/getVisitorCount")
+        .then((getresponse)=>{
+            //validate the status code 
+            expect(getresponse.status).to.eq(200);
+            //validate the response body
+            expect(getresponse.body).to.have.property('total_visitor_count')
+        });
+        cy.request('POST',"https://3hj9vc9684.execute-api.us-west-1.amazonaws.com/dev/incrementVisitorCount")
+        .then((postresponse1)=>{
+            //validate the status code 
+            expect(postresponse1.status).to.eq(200);
+            //validate ip address
+            expect(postresponse1.body).to.deep.eq({message: "Visitor count already updated"});  
+        });
+        cy.request('GET',"https://3hj9vc9684.execute-api.us-west-1.amazonaws.com/dev/getVisitorCount")
+        .then((getresponse1)=>{
+            //validate the status code 
+            expect(getresponse1.status).to.eq(200);
+            //validate the response body
+            expect(getresponse1.body.total_visitor_count).to.eq(getresponse1.body.total_visitor_count)
+        });
+    })
+
+    it("fail Post response", () =>{
+        cy.request({
+            method:'POST',
+            url:"https://3hj9vc9684.execute-api.us-west-1.amazonaws.com/dev/incrementisitorCount",
+            failOnStatusCode:false
+        })
+        .then((response) => {
+            //validate the status code 
+            expect(response.status).to.eq(403);
+        });
+    });
+
+})
